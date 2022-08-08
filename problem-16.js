352da54b8def212bc8434565721c1f3e08bf7a53
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-08 21:47:42 +0600

    problem 16 solved

---
+++ problem-16.js
@@ -0,0 +1,25 @@
+/*
+
+ত োমার কাছে : ৮০০০০ টাকার বে শি হলে তুমি mac কি নবে , ৬০ টাকার বে শি হলে gaming ল্যাপটপ
+কি নবে , ৪০ হাজার টাকার বে শি হলে lenovo yoga কম্পি উটার কি নবে , ২০ হাজার টাকার বে শি হলে পুরান
+ল্যাপটপ কি নবে । না হয় তুমি ম োবাইল দি য়ে কাজ চালাবে ।
+
+*/
+
+let myBudget = 55000;
+
+if (myBudget > 80000) {
+    console.log('Buy Mac')
+}
+else if (myBudget > 60000) {
+    console.log('Buy Gaming Laptop')
+}
+else if (myBudget > 40000) {
+    console.log('Buy Lenovo yoga')
+}
+else if (myBudget > 20000) {
+    console.log('Buy old Laptop')
+}
+else {
+    console.log('I am happy with mobile.')
+}
