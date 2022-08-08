4f023c0adf4830c3beb776dfcade50995d2e18c1
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-08 22:02:10 +0600

    problem 23 solved

---
+++ problem-23.js
@@ -0,0 +1,12 @@
+/*
+তুমি এতদি ন পর্যন্ত যে যে মডে লে র ম োবাইল ফ োন ইউজ করে ছ ো সে গুলার নাম দি য়ে একটা
+array বানাও। তারপর একটা while লপু দি য়ে সে ই array এর উপাদান গুলা একটা একটা করে
+আউটপুট হি সে বে দে খাও
+*/
+let phoneList = ['Nokia', 'Samsung', 'Symphony', 'Walton', 'Vivo']
+
+let i = 0; 
+while (i < phoneList.length)
+    {
+    console.log(phoneList[i])
+    i++}
