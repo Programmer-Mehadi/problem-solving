a407ed2f0ecb8bc69d2324ae7faf32121f38ec58
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-08 22:34:13 +0600

    problem 32 solved

---
+++ problem-32.js
@@ -0,0 +1,12 @@
+/*
+একটা ফাংশন লি খবা যে টা যে ক োন নামকে uppercase বা রে গুলার কে ইস হি সে বে নি বে আর
+আউটপুট হি সে বে সে ই নাম lowercase করে রি টার্ন করবে । 
+
+*/
+
+
+function converter(name) {
+    return name.toLowerCase();
+}
+
+console.log(converter("MEHADI"))
