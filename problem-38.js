829ad983daa7447f3ea5e906b08f2a39e90617b7
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-08 22:51:10 +0600

    problem 38 solved

---
+++ problem-38.js
@@ -0,0 +1,17 @@
+/*
+leapYear() নামে ফাংশন লি খ ো এবং নে ক্সট ইয়ার অর্থাৎ ২০২৩ কি leap year নাকি সে টা
+চে ক কর ো। Leap year হলে ফাংশন true রিটার্ন করবে আর leap year না হলে false রিটার্ন
+করবে ।
+*/
+
+function leapYear(year) {
+    if (year % 4 == 0 || year % 400 == 0)
+    {
+        return true;
+    }
+    else {
+        return false
+    }
+}
+
+console.log(leapYear(2023))
