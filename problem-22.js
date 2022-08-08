e1a600699ee754953a3a7aa8e5136554f70397f7
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-08 21:59:19 +0600

    problem 22 solved

---
+++ problem-22.js
@@ -0,0 +1,11 @@
+/* 
+
+তুমি এতদি ন যা যা জি নি স শি খছ ো সে গুলার নাম দি য়ে একটা array বানাও। তারপর একটা
+for লপু দি য়ে সে ই array এর সব উপাদান কে আউটপুট হি সে বে দে খাও।
+
+*/
+
+let learn = ['variable', 'loop', 'array']
+
+for (let i = 0; i < learn.length; i++)
+        console.log(learn[i])
