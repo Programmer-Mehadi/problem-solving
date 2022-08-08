7135cac1453ae62bb6cd016f4deb96940b97f96a
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-08 21:22:19 +0600

    problem 9 solved

---
+++ problem-9.js
@@ -0,0 +1,13 @@
+/**
+ You are given a triangle with the sides 9, 8, 9. Write a program to check
+whether a triangle is Isosceles or not using if-else.
+
+Isosceles => two sides are equal
+ */
+let a = 9, b = 8, c = 9;
+if (a == b || a == c) {
+    console.log('Isosceles')
+}
+else{
+    console.log('Not Isosceles')
+}
