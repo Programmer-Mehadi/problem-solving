b0ceac0d93e958f45c3f796a5132b6f57308e6aa
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-08 21:03:19 +0600

    problem 5 solved

---
+++ problem-5.js
@@ -0,0 +1,33 @@
+/*
+
+নি চে র ভে রি য়ে বল ডি ক্লে য়ার এ ক োনটার মধ্যে কি কি সমস্যা আছে । দেখত ো বের করতে
+পার ো কি না?
+Var price = 33
+var name - Shabana
+var boxName = ‘Cocola;
+var 88_price = 34;
+var enum = -1;
+var _$box’78 = ‘Monika’;
+var home-address = “kochu khet”;
+
+*/
+
+//var name - Shabana
+// problem:  not using = and string must be ""
+var name = "Shabana"
+//------
+//var boxName = ‘Cocola;
+// problem: string '' missing
+var boxName = 'Cocola';
+///------
+//var 88_price = 34;
+//problem: start variable using digit 
+var price_88 = 34;
+//-----
+//var _$box’78 = 'Monika';
+//problem:  variable has '
+var _$box_78 = 'Monika';
+//----
+//var home-address = “kochu khet”;
+//problem: cann't use -
+var home_address = "kochu khet";
