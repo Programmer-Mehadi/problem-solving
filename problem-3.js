93f2b6195d5e4f58d295d46e223d85b9454f8020
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-08 20:52:22 +0600

    problem 3 solved

---
+++ problem-3.js
@@ -0,0 +1,13 @@
+/*
+John’s teacher gave him two variables. Each variable contains a string.
+John’s teacher asked him to combine these two strings(‘I am going to
+be’ and ‘an awesome web developer’) and print them in one line. Help
+John write the program.
+ */
+
+let stringOne= "I am going to be";
+let stringTwo = "an awesome web developer";
+
+let finalString = stringOne +" "+ stringTwo;
+
+console.log(finalString);
