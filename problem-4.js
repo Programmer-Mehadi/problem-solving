148742e0af88bcf1da97f613dc3f56a874f71f1c
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-08 20:56:32 +0600

    problem 4 solved

---
+++ problem-4.js
@@ -0,0 +1,11 @@
+/*
+Sarah’s mother is teaching her mathematics. She gave Sarah the number
+119 and asked her what the remainder would be if she divided the number
+by 5. Help Sarah write the program.
+*/
+
+let number = 119;
+let divider = 5;
+
+let remainder = number % divider;
+console.log("119 % 5 = ",remainder)
