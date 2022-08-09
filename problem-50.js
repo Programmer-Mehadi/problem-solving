08a1ff6c4a519d8ab43b59edef5a61204e3ccf75
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-09 20:36:59 +0600

    problem 50 solved

---
+++ problem-50.js
@@ -0,0 +1,12 @@
+/*
+Write a function which takes the height and width of a rectangle as parameters. Find out the area of that rectangle and print the result.
+*/
+function rectangleArea(h, w) {
+    let area = h * w
+    return area
+}
+
+let height = 4;
+let width =6
+let area = rectangleArea(height, width)
+console.log(area)
