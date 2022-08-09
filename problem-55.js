9b965e10ce589a6a2b9a3cdf02585164d2298ce3
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-09 21:02:33 +0600

    problem 55 solved

--- problem-55.js
+++ problem-55.js
@@ -1,27 +1,8 @@
 /*
-Suppose, you have an array with 8 elements. Find the smallest element of
-that array.
+Try to find the second largest element in a array.
 */
-
-function smallestNumber(array) {
-    let smallValue = array[0]
-    for (let i = 0; i < array.length; i++) {
-        if (smallValue > array[i]) {
-            smallValue = array[i]
-        }
-    }
-    return smallValue
-}
 
 let numbers = [12, 34, 56, 8, 27, -72, 45, 67, 90]
-
-let smallest = smallestNumber(numbers)
-console.log(smallest)
-
-
-/*
-Now for the previous array, try to find the second largest element.
-*/
 
 function secondLargest(array) {
     let large = array[0]
@@ -30,7 +11,7 @@ function secondLargest(array) {
             large =array[i]
         }
     }
-   
+ 
     let secondLarge = array[0]
     for (let i = 0; i < array.length; i++){
         if (secondLarge < array[i] && array[i] != large) {
