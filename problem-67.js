812c10ea2ad522a8efec4bc5951373d46c58a064
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-09 10:16:18 +0600

    problem 67 solved

---
+++ problem-67.js
@@ -0,0 +1,12 @@
+/*
+একটা অবজে ক্ট ডি ক্লে য়ার করবে । সে টাতে তি নটা প্রপার্টি থাকবে । এবং ক োন একটা প্র োপার্টি
+এর মান চে ইঞ্জ করবা।
+*/
+const ageList = {
+    "Sabur": 25,
+    "Sabila": 21,
+    "Nur":28
+}
+console.log(ageList)
+ageList.Sabila=19
+console.log(ageList)
