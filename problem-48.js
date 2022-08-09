d6345ac74a74a20932f528b7a69f5b4f2857d1e8
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-09 20:28:33 +0600

    problem 48 solved

--- problem-48.js
+++ problem-48.js
@@ -13,7 +13,32 @@ function smallestNumber(array) {
     return smallValue
 }
 
-let numbers = [12, 34, 56, 78, 27, -72, 45, 67, 90]
+let numbers = [12, 34, 56, 8, 27, -72, 45, 67, 90]
 
 let smallest = smallestNumber(numbers)
 console.log(smallest)
+
+
+/*
+Now for the previous array, try to find the second largest element.
+*/
+
+function secondLargest(array) {
+    let large = array[0]
+    for (let i = 0; i < array.length; i++){
+        if (large < array[i]) {
+            large =array[i]
+        }
+    }
+    console.log(large)
+    let secondLarge = array[0]
+    for (let i = 0; i < array.length; i++){
+        if (secondLarge < array[i] && array[i] != large) {
+            secondLarge=array[i]
+        }
+    }
+    return secondLarge
+}
+
+let secondlargeValue = secondLargest(numbers)
+console.log(secondlargeValue)
