150fb3c177af2b40b7cfa916d59a2b2a0fc97c85
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-09 20:10:54 +0600

    problem 47 solved

---
+++ problem-47.js
@@ -0,0 +1,16 @@
+/*
+সুদে র হি সাব করবে । জাস্ট হি সাব কে মনে করতে হয়। সে ই চি ন্তায় করবে । সুদ ভাল ো না খারাপ
+সে টা এখন চি ন্তা করার দরকার নাই। জাস্ট একটা ফর্মুলর্মু া থাকলে সে টা কি ভাবে ক োড এ লি খতে হয়
+সে ই এঙ্গে ল থে কে করার চে ষ্টা কর ো।
+*/
+function calculator(totalMoney,years,percent) {
+    
+    let perYearMoney = 23400 + ((23400 * percent) / 100) * year
+    return perYearMoney  
+
+}
+
+let money = 23400;
+let year = 4;
+let percentages = 9;
+console.log(calculator(money,year,percentages))
