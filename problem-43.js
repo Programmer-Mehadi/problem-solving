bd1a8b9ca09d222f49cfb6fa7043cda32a4bd570
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-08-09 19:41:39 +0600

    problem 43 solved

---
+++ problem-43.js
@@ -0,0 +1,11 @@
+/*
+তিনটা সংখ্যা এর য োগ করতে পারবে এমন একটা ফাংশন লি োখ
+*/
+
+function summation(n1, n2, n3) {
+   let sum =n1+n2+n3
+    return sum
+}
+
+let total = summation(3,45,2)
+console.log(total)
